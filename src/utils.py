679e4d6cbb1a557bd80daae4fb9a82ff9bc966be
rb58853 <rb58853@gmail.com>
2024-05-02 09:49:17 -0400

    add model_name to EmbeddingModel

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 class EmbeddingModel:
     def __init__(self, model_name):
+        self.model_name = model_name
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModel.from_pretrained(model_name)
 
